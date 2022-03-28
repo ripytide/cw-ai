e956f4996b5daa71b6dfe72b368ed9dea573b971
olliemamuda <olliemamuda@gmail.com>
2022-03-28 16:07:07 +0100

    using minimax to pick the best move (depth 1)

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MinMax.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MinMax.java
@@ -5,25 +5,39 @@ import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 
 import javax.annotation.Nonnull;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class MinMax implements MovePicker {
     @Override
     public Move selectionAlgorithm(@Nonnull Board board, Pair<Long, TimeUnit> timeoutPair, Score scoringMethod) {
+        Long currentTime = Instant.now().toEpochMilli();
+        Long endTime = currentTime + timeoutPair.right().toMillis(timeoutPair.left());
+
         Move bestMove;
-        LinkedList<Move> moveQueue = new LinkedList<>(board.getAvailableMoves());
+        Float bestScore = 0f;
+        for(Move move : board.getAvailableMoves()){
+            Float currentScore = miniMax(((Board.GameState)board).advance(move), endTime, scoringMethod, true, 1);
+            if(currentScore > bestScore){
+                bestMove = move;
+                bestScore = currentScore;
+            }
+        }
     }
 
-    private static Optional<Float> miniMax(@Nonnull Board.GameState gameState, Pair<Long, TimeUnit> timeoutPair, Score scoringMethod, boolean maxing, Integer depth) {
-        if (stillGotTime) {
+    private static Optional<Float> miniMax(@Nonnull Board.GameState gameState, Long endTime, Score scoringMethod, boolean maxing, Integer depth) {
+        Long currentTime = Instant.now().toEpochMilli();
+        if (currentTime < endTime) {
             if (depth <= 0 || !gameState.getWinner().isEmpty()) {
                 return Optional.of(scoringMethod.score(gameState));
             }
             Float bestValue = maxing ? 0f : 1f;
             for (Move move : gameState.getAvailableMoves()) {
-                Optional<Float> value = miniMax(gameState.advance(move), timeoutPair, scoringMethod, !maxing, depth--);
+                Optional<Float> value = miniMax(gameState.advance(move), endTime, scoringMethod, !maxing, depth--);
                 if (value.isEmpty()) {
                     return Optional.empty();
                 } else {
