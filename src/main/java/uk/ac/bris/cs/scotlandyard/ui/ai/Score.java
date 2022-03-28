a38f6ca57d0710b29e657f753058be7e3a40e6d1
olliemamuda <olliemamuda@gmail.com>
2022-03-28 13:57:07 +0100

    writing SimpleScore

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Score.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Score.java
@@ -3,5 +3,5 @@ package uk.ac.bris.cs.scotlandyard.ui.ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
 
 public interface Score {
-    public Integer score(Board.GameState gameState);
+    public float score(Board.GameState gameState);
 }
--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/SimpleScore.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/SimpleScore.java
@@ -1,10 +1,21 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
+
+import java.util.stream.Collectors;
 
 public class SimpleScore implements Score{
     @Override
-    public Integer score(Board.GameState gameState) {
-        return null;
+    public float score(Board.GameState gameState) {
+        ImmutableSet<Piece> winners = gameState.getWinner();
+        if(winners.isEmpty()){
+            return 0.5f;
+        }else if(winners.stream().noneMatch(Piece::isMrX)){
+            return 0f;
+        }else{
+            return 1;
+        }
     }
 }
