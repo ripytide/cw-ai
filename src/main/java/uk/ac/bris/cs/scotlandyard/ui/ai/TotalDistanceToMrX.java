7ec26e79f0fd8bedb2ba44b1b7a46ba4595d1ab7
olliemamuda <olliemamuda@gmail.com>
2022-04-21 18:11:13 +0100

    continuing debugging detectiveAI

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/TotalDistanceToMrX.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/TotalDistanceToMrX.java
@@ -6,6 +6,10 @@ public class TotalDistanceToMrX implements Score {
 
     @Override
     public Float score(CustomGameState gameState) {
+        List<Float> distances = gameState.getDistancesBetweenMrXAndDetectives();
+        if(distances.stream().reduce(0f, Float::sum) == 1.0f){
+            System.out.println(distances);
+        }
         return gameState.getDistancesBetweenMrXAndDetectives().stream().reduce(0f, Float::sum);
     }
 }
